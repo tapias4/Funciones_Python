1e84f8dca120a4e46eeeacc740d639a8cf6e2159
tapias4 <diegotapias@colegioguanenta.edu.cogit config user.email diegotapias@colegioguanenta.edu.co>
2025-03-12 07:54:43 -0500

    actualizacion

--- Sin_funciones.py
+++ Sin_funciones.py
@@ -9,5 +9,14 @@ b = int(input("Numero a buscar: "))  # Se recibe el dato del usuario
 a = [1, 2, 3, 4, 5] # Se almacena una lista de datos
 r = False # Se inicia la variable r con un valor falso
 
+for i in a:
+    if i==b:
+        print("Lo encontre")
+        r= True
+    if r==False:
+        print("No la encontre")    
 
+
+# salida
+print("\nEso era ...")        
 
